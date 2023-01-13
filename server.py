2a45227057b30f80e7474266e134b75bd55351f2
radioval87 <radioval87@yandex.ru>
2023-01-13 16:48:02 +0300

    Add urls number limit for a single analysis

--- server.py
+++ server.py
@@ -7,14 +7,15 @@ from main import main as process_articles
 async def get_articles_stats(request: Request):
     urls = []
     if request.rel_url.query['urls']:
-        for url in request.rel_url.query['urls'].split(','):
-            urls.append(url.strip())
-    
-    if len(urls) > 10:
-        return web.json_response(data={
-            "error": "too many urls in request, should be 10 or less"
-            }, status=400
-        )
+        urls = request.rel_url.query['urls'].split(',')
+        if len(urls) > 10:
+            return web.json_response(
+                data={
+                    "error": "too many urls in request, should be 10 or less"
+                }, status=400
+            )
+        for url in urls:
+            url = url.strip()
 
     results = await process_articles(urls)
     response = {
