a5c10fd15ba985bc9562e6a87cbcefa4299be920
radioval87 <radioval87@yandex.ru>
2023-02-11 21:04:17 +0300

    Replace urls strip with replace

--- server.py
+++ server.py
@@ -7,15 +7,13 @@ from articles_processor import process_articles
 async def get_articles_stats(request: Request):
     urls = []
     if 'urls' in request.rel_url.query and request.rel_url.query['urls']:
-        urls = request.rel_url.query['urls'].split(',')
+        urls = request.rel_url.query['urls'].replace(' ', '').split(',')
         if len(urls) > 10:
             return web.json_response(
                 data={
                     "error": "too many urls in request, should be 10 or less"
                 }, status=400
             )
-        for url in urls:
-            url = url.strip()
 
     results = await process_articles(urls)
     response = {
